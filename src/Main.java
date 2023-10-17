a1299e3e510fbd784edb46493aae2903685785e0
g21turbo <dg21stew@gmail.com>
2023-10-16 21:02:13 -0700

    added else if

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,35 @@ public class Main {
     public static void main(String[] args) {
 
         /*
-            if (condition) {
+            if (firstCondition) {
                 Code in block will execute only if
                 condition is true
 
+            } else if (secondCondition) {
+                Code in block will excecute if firstCondition is false
+                and secondCondition is true
+
                 block can contain 1 or many statements
-            }
+
+
+            } else {
+                Code in block will execute if
+                all conditions above are false
+
+
          */
 
         boolean gameOver = true;
-        int score = 5001;
+        int score = 800;
         int levelCompleted = 5;
         int bonus = 100;
 
-        if (score <= 5000) {
-            System.out.println("Your score is 5000 or less");
+        if (score < 5000 && score > 1000) {
+            System.out.println("Your score is less than 5000 but greater than 1000");
+        } else if (score < 1000){
+            System.out.println("Your score is less than 1000");
         } else {
-            System.out.println("You scored 5000 or higher");
+            System.out.println("Got here");
         }
 
     }
